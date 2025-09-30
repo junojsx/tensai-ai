817369997a83b581f473e9469cba33bd702185be
junojsx <junojsx@gmail.com>
2025-09-29 22:27:39 -0700

    adding dashboard and landing page

---
+++ app/(dashboard)/(routes)/(landing)/page.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return ( 
+        <>
+        <h1 className="text-6xl text-green-500">Hello Tensai!</h1>
+        </>
+     );
+}
+ 
+export default LandingPage;
---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+const DashboardPage = () => {
+    return ( 
+    <>
+    <h1 className="text-4xl text-blue-500">Dashboard Page</h1>
+    </> );
+}
+ 
+export default DashboardPage;
