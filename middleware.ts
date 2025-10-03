9ca0d277e67b22ebebea175b5a34756689511fdd
junojsx <junojsx@gmail.com>
2025-10-02 21:52:04 -0700

    configuring middleware for protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
@@ -10,3 +16,4 @@ export const config = {
     "/(api|trpc)(.*)",
   ],
 };
+887;
