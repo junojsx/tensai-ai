a2d2ec3969e084765747d2b9878c70178fe5220b
junojsx <junojsx@gmail.com>
2025-10-04 12:56:10 -0700

    adding userbutton to navbar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { SignOutButton, UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
   return (
     <>
       <h1 className="text-4xl text-blue-500">Dashboard Page</h1>
-      <UserButton />
     </>
   );
 };
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,16 +1,23 @@
 import Link from "next/link";
-import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import { Menu } from "lucide-react";
+import { Button } from "./button";
+import { UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
-    <div className="flex-items-center p-4">
-      Menu
-      <Link
-        href="/sign-in"
-        className="border-2 border-solid p-2 m-2 rounded-lg bg-blue-700 text-white"
+    <div className="flex items-center p-4">
+      <Button
+        className="md:hidden"
+        variant={"ghost"}
+        aria-label="Menu"
+        aria-expanded="false"
+        aria-controls="sidebar"
       >
-        {" "}
-      </Link>
+        <Menu className="h-6 w-6 cursor-pointer" />
+      </Button>
+      <div className="flex w-full justify-end">
+        <UserButton />
+      </div>
     </div>
   );
 };
