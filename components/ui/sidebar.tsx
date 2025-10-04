89d4cb99ca77d7410c7ffd0f349bf74fb0a2735c
junojsx <junojsx@gmail.com>
2025-10-04 13:21:07 -0700

    adding logo

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -7,12 +7,14 @@ const Sidebar = () => {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       {" "}
-      <Link href="/" className="block py-2 px-4 rounded hover:bg-gray-700">
-        <div className="relative w-8 h-8 mr-4">
-          <Image fill alt="logo" src="/file.svg" />
-        </div>
-      </Link>
       <div className="px-3 py-2 flex-1">
+        <Link href="/" className="flex items-center mb-14 pl-3">
+          <div className="relative w-30 h-30 mr-4">
+            <Image fill alt="logo" src="/logo.png" />
+          </div>
+          <h2 className="text-2xl font-bold">Tensai</h2>
+        </Link>
+
         <Link
           href="/dashboard"
           className="block py-2 px-4 rounded hover:bg-gray-700"
