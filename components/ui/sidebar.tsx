3dccbf0bf72185475c9197a996daf9b98f6a7df2
junojsx <junojsx@gmail.com>
2025-10-06 19:41:08 -0700

    additional routes and icons for sidebar links

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -4,7 +4,15 @@ import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import { LayoutDashboard } from "lucide-react";
+import {
+  Code2Icon,
+  ImageIcon,
+  LayoutDashboard,
+  MessageSquare,
+  Music2Icon,
+  Settings2Icon,
+  VideoIcon,
+} from "lucide-react";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 const routes = [
@@ -13,6 +21,42 @@ const routes = [
     href: "/dashboard",
     icon: LayoutDashboard,
     color: "text-blue-500",
+  },
+  {
+    label: "Conversation",
+    href: "/dashboard",
+    icon: MessageSquare,
+    color: "text-pink-500",
+  },
+  {
+    label: "Generate Video",
+    href: "/dashboard",
+    icon: VideoIcon,
+    color: "text-green-500",
+  },
+  {
+    label: "Generate Image",
+    href: "/dashboard",
+    icon: ImageIcon,
+    color: "text-red-500",
+  },
+  {
+    label: "Generate Music",
+    href: "/dashboard",
+    icon: Music2Icon,
+    color: "text-purple-500",
+  },
+  {
+    label: "Generate Code Pattern",
+    href: "/dashboard",
+    icon: Code2Icon,
+    color: "text-orange-500",
+  },
+  {
+    label: "Settings",
+    href: "/dashboard",
+    icon: Settings2Icon,
+    color: "text-gray-500",
   },
 ];
 
@@ -31,7 +75,11 @@ const Sidebar = () => {
         </Link>
         <div className="space-y-1">
           {routes.map((route) => (
-            <Link href={route.href} key={route.href}>
+            <Link
+              href={route.href}
+              key={route.href}
+              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+            >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
                 <span className="font-medium">{route.label}</span>
