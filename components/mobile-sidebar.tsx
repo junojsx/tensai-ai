429a5b79c6758423af5a59da5a92be2dba1e91d6
junojsx <junojsx@gmail.com>
2025-10-06 19:52:00 -0700

    adding flyout menu

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,20 +1,28 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "./sidebar";
 
 const MobileSidebar = () => {
   return (
-    <>
-      <Button
-        className="md:hidden"
-        variant={"ghost"}
-        aria-label="Menu"
-        aria-expanded="false"
-        aria-controls="sidebar"
-      >
-        <Menu className="h-6 w-6 cursor-pointer" />
-      </Button>
-    </>
+    <Sheet>
+      <SheetTrigger>
+        <Button
+          className="md:hidden"
+          variant={"ghost"}
+          aria-label="Menu"
+          aria-expanded="false"
+          aria-controls="sidebar"
+        >
+          <Menu className="h-6 w-6 cursor-pointer" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        {/* @ts-ignore */}
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
   );
 };
 
