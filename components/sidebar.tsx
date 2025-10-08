a5535a129dc160b8761a5e7cc4673176e23cbb7a
junojsx <junojsx@gmail.com>
2025-10-07 21:31:45 -0700

    adding pathnames

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -13,6 +13,8 @@ import {
   Settings2Icon,
   VideoIcon,
 } from "lucide-react";
+import { usePathname } from "next/navigation";
+import path from "path";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 const routes = [
@@ -61,6 +63,9 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
+  //only show routes that are not the current route
+  // const filteredRoutes = routes.filter((route) => route.href !== pathname);
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       {" "}
@@ -78,7 +83,13 @@ const Sidebar = () => {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400",
+                montserrat.className
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
