76f4a6662d50b68ae5224e902bf27dbda90d0651
junojsx <junojsx@gmail.com>
2025-10-07 21:38:21 -0700

    adding headers

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,29 @@
+"use client"; //we have to use client for interactive components
+
+const tools = [
+  {
+    label: "Chat",
+    icon: "MessageSquare",
+    href: "/converesation",
+    color: "text-violet-500/10 bg-sky-500/20",
+  },
+];
 const DashboardPage = () => {
   return (
     <>
-      <h1 className="text-4xl text-blue-500">Dashboard Page</h1>
+      <div className="mb-8 space-y-4">
+        <h1 className="text-2xl md:text-4xl font-bold text-center">
+          Tensai AI Assistant
+        </h1>
+        <h2 className="text-sm text-muted-foreground font-light md:text-lg text-center">
+          Chat with your personal AI assistant.
+        </h2>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <a href={tool.href}></a>
+        ))}
+      </div>
     </>
   );
 };
